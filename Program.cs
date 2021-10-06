8ed7695bf1ed1f60e74c24ba87d01c9ccae16b79
Ulyana Shabunina <66304635+UlyanaShabunina@users.noreply.github.com>
2021-10-06 22:45:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,35 +12,27 @@ namespace Task2
             String line;
             try
             {
-                //Передайте путь к файлу и имя файла конструктору StreamReader
                 StreamReader sr = new StreamReader("..\\..\\input.txt");
-                //Прочитайте первую строку текста
                 line = sr.ReadLine();
-                //Продолжайте читать, пока не дойдете до конца файла
                 while (line != null)
                 {
-                    //напишите строку в окно консоли
                     Console.WriteLine(line);
-                    //Прочтите следующую строчку
                     line = sr.ReadLine();
                 }
-
+                
                 StreamWriter sw = new StreamWriter("..\\..\\output.txt");
                
-                //Close the file
                 sr.Close();
                 sw.Close();
                 File.Copy("..\\..\\input.txt", "..\\..\\output.txt", true);
                 File.WriteAllText("..\\..\\output.txt", File.ReadAllText("..\\..\\output.txt", Encoding.Default).Replace(". ", "\n"));
                 
-
                 Console.ReadLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-
         }
     }
 }
